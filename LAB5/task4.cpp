b0744096e107bc2f12b014335d1b1fe9f0731194
Abdullah-AYP <abdullahyousuf1836@gmail.com>
2025-03-07 16:59:50 +0500

    Update task4.cpp

--- LAB5/task4.cpp
+++ LAB5/task4.cpp
@@ -1,1 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class AlarmSystem {
+private:
+    string security_level;
+
+public:
+    AlarmSystem(const string& level = "Medium") : security_level(level) {
+        cout << "AlarmSystem created with " << security_level << " security level" << endl;
+    }
+    void displayInfo() {
+        cout << "Security Level: " << security_level << endl;
+    }
+    void setSecurityLevel(const string& level) {
+        security_level = level;
+    }
+    ~AlarmSystem() {
+        cout << "AlarmSystem is being destroyed" << endl;
+    }
+};
+class SmartHome {
+private:
+    string name;
+    string location;
+    AlarmSystem alarm_system;  
+
+public:
+    SmartHome(const string& homeN, const string& homeL, const string& securityLevel = "Medium")
+        : name(homeN), location(homeL), alarm_system(securityLevel) {}
+
+    void updateSecurity(const string& level) {
+        alarm_system.setSecurityLevel(level);
+        cout << "Alarm security level updated to " << level << endl;
+    }
+    void displayDetails() {
+        cout << "Smart Home: " << name << endl;
+        cout << "Location: " << location << endl;
+        cout << "Alarm System: ";
+        alarm_system.displayInfo();
+        cout << endl;
+    }
+    ~SmartHome() {
+        cout << "SmartHome '" << name << "' is being destroyed" << endl;
+    }
+};
+int main() {
+    SmartHome home1("Bilawal House", "Clifton");
+    home1.displayDetails();
+ 
+    home1.updateSecurity("High");
+    home1.displayDetails();
+    
+    SmartHome home2("Lodi's House", "Gulshan-e-Iqbal", "Low");
+    home2.displayDetails();
+
+    cout << "Program ending....\n" << endl;
+    return 0;
+}
 

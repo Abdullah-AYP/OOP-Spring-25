ba557d925e9c225701fe101db1086eec5ec2ed3d
Abdullah-AYP <abdullahyousuf1836@gmail.com>
2025-05-05 21:55:54 +0500

    Update task8.cpp

--- LAB10/task8.cpp
+++ LAB10/task8.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    fstream config("config.txt", ios::in | ios::out);
+    if (!config.is_open()) {
+        cerr << "Error: Could not open config.txt" << endl;
+        return 1;
+    }
+    config.seekp(5);
+    config.write("babar", 5); 
+
+    config.seekg(0);
+    string updated;
+    getline(config,updated);
+    cout << "New Config Line: " << updated << endl;
+
+    config.close();
+    return 0;
+}
 

3c707c9f8faf71794be740b4150e7c9ea59e1809
Abdullah-AYP <abdullahyousuf1836@gmail.com>
2025-05-05 21:54:54 +0500

    Update task6.cpp

--- LAB10/task6.cpp
+++ LAB10/task6.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+struct InventoryItem {
+    int itemID;
+    char itemName[20];
+};
+int main() {
+    InventoryItem i1 = {101, "WrenchSet"};
+    ofstream out("inventory.dat", ios::binary);
+    out.write((char*)&i1, sizeof(i1));
+    out.close();
+    cout << "Item written to inventory.dat\n";
+
+    InventoryItem l1;
+    ifstream in("inventory.dat", ios::binary);
+    in.read((char*)&l1, sizeof(l1));
+    in.close();
+
+    cout << "Item read from file:\n";
+    cout << "ID: " << l1.itemID << ", Name: " << l1.itemName << endl;
+
+    return 0;
+}

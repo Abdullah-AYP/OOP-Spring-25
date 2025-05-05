13666dee43bb21010eced91883f1c7150b402048
Abdullah-AYP <abdullahyousuf1836@gmail.com>
2025-05-05 22:03:09 +0500

    Update task9.cpp

--- LAB10/task9.cpp
+++ LAB10/task9.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+int main() {
+    ifstream large("large_log.txt");
+    if (!large.is_open()) {
+        cerr << "Error: Could not open large_log.txt" << endl;
+        return 1;
+    }
+    char buffer[11] = {0};
+
+    streampos pos1 = large.tellg();
+    large.read(buffer, 10);
+    cout << "First read: " << buffer << endl;
+    cout << "Position before first read: " << pos1 << endl;
+    cout << "Position after first read: " << large.tellg() << endl;
+
+    streampos pos2 = large.tellg();
+    large.read(buffer, 10);
+    cout << "Second read: " << buffer << endl;
+    cout << "Position before second read: " << pos2 << endl;
+    cout << "Position after second read: " << large.tellg() << endl;
+
+    large.close();
+    return 0;
+}

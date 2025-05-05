2e5da5923d0381551e488ff38a7e23decc4f1290
Abdullah-AYP <abdullahyousuf1836@gmail.com>
2025-05-05 21:55:17 +0500

    Update task7.cpp

--- LAB10/task7.cpp
+++ LAB10/task7.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+int main() {
+    ofstream log("sensor_log.txt");
+
+    streampos pos1 = log.tellp();
+    log << "Sensor A: 27.3 C\n";
+    cout << "Position before Sensor A: " << pos1 << " bytes\n";
+
+    streampos pos2 = log.tellp();
+    log << "Sensor B: 45% RH\n";
+    cout << "Position before Sensor B: " << pos2 << " bytes\n";
+
+    streampos pos3 = log.tellp();
+    cout << "Final write position: " << pos3 << " bytes\n";
+
+    log.close();
+    return 0;
+}

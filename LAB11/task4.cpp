32b644ad5f781bc60070ca3b9facbfb10aa1fe13
Abdullah-AYP <abdullahyousuf1836@gmail.com>
2025-05-11 21:10:33 +0500

    Update task4.cpp

--- LAB11/task4.cpp
+++ LAB11/task4.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <exception>
+using namespace std;
 
+class DatabaseException : public exception {
+public:
+    const char* what() const noexcept override {
+        return "General database error.";
+    }
+};
+class ConnectionFailedException : public DatabaseException {
+public:
+    const char* what() const noexcept override {
+        return "Failed to connect to database.";
+    }
+};
+template <typename T>
+class DatabaseConnector {
+public:
+    void connect(T dbName) {
+        if (dbName == "invalid")
+            throw ConnectionFailedException();
+        throw DatabaseException();
+    }
+};
+int main() {
+    DatabaseConnector<string> db;
+
+    try {
+        db.connect("invalid"); 
+    } catch (const ConnectionFailedException& e) {
+        cout << "Caught: " << e.what() << endl;
+    }
+    try {
+        db.connect("error"); 
+    } catch (const DatabaseException& e) {
+        cout << "Caught: " << e.what() << endl;
+    }
+    return 0;
+}
